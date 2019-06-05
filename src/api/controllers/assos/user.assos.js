a98ef16dce9205d208ecc717ebd4b69031e236e7
Arnaud9145 <arnaud.dufour@utt.fr>
2019-06-05 16:13:52 +0200

    added assos route

---
+++ src/api/controllers/assos/user.assos.js
@@ -0,0 +1,59 @@
+const errorHandler = require('../../utils/errorHandler')
+const isAuth = require('../../middlewares/isAuth')
+const axios = require('axios')
+
+module.exports = app => {
+  app.get('/assos', [isAuth('assos')])
+  app.get('/assos', async (req, res) => {
+    try {
+      const result = await axios.get(
+        `${process.env.ETU_BASEURL}/api/private/user/organizations`,
+        {
+          headers: { Authorization: `Bearer ${req.user.access_token}` }
+        }
+      )
+      let assos = result.data.data.map(asso => {
+        return {
+          login: asso._embed.organization,
+          role: asso.role,
+          permissions: asso.permissions,
+          link: asso._links.find(l => l.rel === 'member.organization').uri
+        }
+      })
+      assos = await Promise.all(
+        assos.map(async asso => {
+          let result2 = null
+          try {
+            result2 = await axios.get(
+              `${process.env.ETU_BASEURL}${asso.link}`,
+              {
+                headers: { Authorization: `Bearer ${req.user.access_token}` }
+              }
+            )
+            const { data } = result2.data
+            return {
+              ...asso,
+              name: data.name,
+              website: data.website,
+              phone: data.phone,
+              image: data._links.find(l => l.rel === 'orga.image').uri,
+              mail: data.mail,
+              decription: data.description,
+              decriptionShort: data.decriptionShort
+            }
+          } catch (e) {
+            console.log('FAILED', asso.login, e)
+          }
+
+          return asso
+        })
+      )
+      return res
+        .status(200)
+        .json(assos)
+        .end()
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  })
+}

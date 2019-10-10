2e1f0a57b51f0206fa5cf116aad745420a40ddf1
Arnaud9145 <arnaud.dufour@utt.fr>
2019-10-10 20:26:26 +0200

    idk

--- src/api/utils/errorHandler.js
+++ src/api/utils/errorHandler.js
@@ -1,4 +1,5 @@
-module.exports = (err, req, res) => {
+const axios = require('axios')
+module.exports = async (err, req, res) => {
   console.log(err)
 
   if (err.name === 'SequelizeUniqueConstraintError') {
@@ -14,22 +15,24 @@ module.exports = (err, req, res) => {
       .end()
   }
   if (err.response && err.response.status === 403) {
+    if (err.response.data && err.response.data.error === 'expired_token') {
+      if (req.user) {
+        try {
+          console.log('REFRESHING')
+          const token = req.user.refresh_token
+          const result = await axios.get(
+            `${process.env.ETU_BASEURL}/api/oauth/authorize?grant_type=refresh_token&refresh_token=${token}&scopes=${process.env.ETU_SCOPE}&client_id=${process.env.ETU_CLIENT_ID}&client_secret=${process.env.ETU_CLIENT_SECRET}&state=xyz`
+          )
+          console.log(result.data)
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    }
     return res
       .status(404)
       .json({ error: err.response.data.error })
       .end()
-  }
-  if (
-    err.response &&
-    err.response.data &&
-    err.response.data.error === 'expired_token'
-  ) {
-    if (req.user) {
-      let token = req.user.refresh_token
-      return res.redirect(
-        `${process.env.ETU_BASEURL}/api/oauth/authorize?grant_type=refresh_token&refresh_token=${token}&scopes=${process.env.ETU_SCOPE}&client_id=${process.env.ETU_CLIENT_ID}&client_secret=${process.env.ETU_CLIENT_SECRET}&state=xyz`
-      )
-    }
   }
 
   return res
